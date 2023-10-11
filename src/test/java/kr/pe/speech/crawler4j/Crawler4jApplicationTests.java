2b6b9f0c40f44aeafe3403a0be53ea4f7e41fca2
sahngwoon_lee <sahngwoon.lee@llsollu.com>
2023-10-12 08:50:02 +0900

    Initial commit

---
+++ src/test/java/kr/pe/speech/crawler4j/Crawler4jApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.speech.crawler4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crawler4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
